2316e8969f38200023d77bf9922a398297ca596d
davidbeck66 <davidbeck@mymail.mines.edu>
2021-03-26 17:55:39 -0600

    added some motivation for the student in grades

--- src/TheGradeMotivator.java
+++ src/TheGradeMotivator.java
@@ -11,6 +11,7 @@ public class TheGradeMotivator {
 	}
 	public void grade() {
 		System.out.println("How are those grades going?");
+		System.out.println("you are going to do great!");
 	}
 	public static void main(String[] args) {
 		TheGradeMotivator tm = new TheGradeMotivator();
